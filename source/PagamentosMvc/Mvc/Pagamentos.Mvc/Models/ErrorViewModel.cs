40b67ad7742b8386a5f490b834524867a50d04ad
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-10-15 00:29:23 -0300

    Create Projeto MVC

---
+++ source/PagamentosMvc/Mvc/Pagamentos.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pagamentos.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
