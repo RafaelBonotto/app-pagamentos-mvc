de564ef3f4c78b6dd1f6dd85c04af4ef5d70c6b9
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-17 00:34:22 -0300

    Update FornecedorViewModel

--- source/PagamentosMvc/Mvc/Pagamentos.Mvc/Models/PagamentoViewModel.cs
+++ source/PagamentosMvc/Mvc/Pagamentos.Mvc/Models/PagamentoViewModel.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Pagamentos.Dominio.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pagamentos.Mvc.Models
 {
     public class PagamentoViewModel
     {
+        public TipoPagamentoEnum TipoPagamento { get; set; } = TipoPagamentoEnum.AVista;
+
+        [Required(ErrorMessage ="O valor do pagamento é obrigatório")]
         public decimal Valor { get; set; }
-        public DateTime Dt_entrega { get; set; }
+
+        public DateTime Dt_compra { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "A data de vencimento do pagamento é obrigatório")]
         public DateTime Dt_vencimento { get; set; }
-        public int Num_parcela { get; set; }
-        public int Qtd_parcela { get; set; }
-        public string Nota_fiscal { get; set; }
-        public string FornecedorDescricao { get; set; }
+
+        public int Num_parcela { get; set; } = 1;
+
+        public int Qtd_parcela { get; set; } = 1;
+
+        public string? Nota_fiscal { get; set; } = null;
+
+        public string? FornecedorDescricao { get; set; } = null;
+
         public List<SelectListItem> FornecedoresBanco { get; set; } = new();
     }
 }
