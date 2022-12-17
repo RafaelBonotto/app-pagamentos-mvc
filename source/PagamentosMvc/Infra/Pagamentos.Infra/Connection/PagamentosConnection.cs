6a7dfa5ba363ad7e3f017928470e26a0cd881f19
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-17 00:47:37 -0300

    Create PagamentosConnection

---
+++ source/PagamentosMvc/Infra/Pagamentos.Infra/Connection/PagamentosConnection.cs
@@ -0,0 +1,13 @@
+﻿using Pagamentos.Infra.Connection.Interfaces;
+using System.Data;
+
+namespace Pagamentos.Infra.Connection
+{
+    public class PagamentosConnection : IPagamentosConnection
+    {
+        public IDbConnection CreateConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
